21d5b9d99889b4c9149543346858d02a5e50ba61
Harnoor Chima <harnoorchima1@gmail.com>
2023-11-04 22:17:49 -0700

    update login.js

--- class-notes-frontend/src/login.js
+++ class-notes-frontend/src/login.js
@@ -1,13 +1,15 @@
 import React, { useRef, useState, useEffect, useContext } from 'react';
 import AuthContext from './context/authProvider';
 import axios from './api/axios';
+import { useHistory } from 'react-router-dom';
 // import { Button, ButtonProps } from '@mantine/core';
-const LOGIN_URL = 'http://localhost:8080/api/routes/user/login';
-const REGISTER_URL = 'http://localhost:8080/api/routes/user/register';
+const LOGIN_URL = 'http://10.0.0.7:8080/api/routes/user/login';
+const REGISTER_URL = 'http://10.0.0.7:8080/api/routes/user/register';
 
 // Gonna need to verify this path b/c its from backend
 
 const Login = () => {
+    const history = useHistory();
     const { setAuth } = useContext(AuthContext);
     const userRef = useRef();
     const errRef = useRef();
@@ -56,6 +58,9 @@ const Login = () => {
                     return ({ "error": "error" });
                 }
             }).then(data => {
+            	if (succ) {
+            	    history.push('/classes');
+            	}
                 console.log(data);
             })
         } catch (error) {
@@ -107,54 +112,54 @@ const Login = () => {
                     </p>
                 </div>
             ) : (
+		<div className ='w-full h-screen bg-gray-500'>
+		        <div className='flex flex-row flex-between w-auto h-full content-center text-2xl bg-gray-500'>
+		            <div className='w-[500px] flex flex-col content-center mx-auto border-solid border-2 h-[600px] p-[30px] bg-zinc-400 rounded-xl mt-[5%]'>
+		                <p ref={errRef} className={errMess ? "errmess" :
+		                    "offscreen"} aria-live="assertive">{errMess}</p>
 
-                <div className='flex flex-row flex-between w-auto content-center text-2xl mt-[50px]'>
-                    <div className='w-[400px] flex flex-col content-center mx-auto border-solid border-2 h-[500px] p-[20px]'>
-                        <p ref={errRef} className={errMess ? "errmess" :
-                            "offscreen"} aria-live="assertive">{errMess}</p>
+		                <h1 className='text-4xl mb-[20px]'>Sign In</h1>
 
-                        <h1 className='text-4xl mb-[20px]'>Sign In</h1>
+		                <form onSubmit={handleSubmit} className='w-2/4 md:w-full flex-col flex h-[300px]'>
+		                    <label htmlFor="username" className='mt-auto mb-2'>Username:</label>
+		                    <input type="text" id="username" className= "border-2 border-black w-7/8 mb-auto h-[50px] rounded mx-auto" ref={userRef} autoComplete="off" onChange={(e) => setUser(e.target.value)} value={user} required />
 
-                        <form onSubmit={handleSubmit} className='w-2/4 md:w-full flex-col flex h-[300px]'>
-                            <label htmlFor="username" className='mt-auto mb-2'>Username:</label>
-                            <input type="text" id="username" className= "border-2 border-black w-7/8 mb-auto h-[50px] rounded mx-auto" ref={userRef} autoComplete="off" onChange={(e) => setUser(e.target.value)} value={user} required />
+		                    <label htmlFor="password" className='mt-auto mb-2'>Password:</label>
+		                    <input type="password" id="password" onChange={(e) => setPassword(e.target.value)} className= "rounded w-7/8 mx-auto border-2 h-[50px] mb-auto  border-black" value={password} required />
 
-                            <label htmlFor="password" className='mt-auto mb-2'>Password:</label>
-                            <input type="password" id="password" onChange={(e) => setPassword(e.target.value)} className= "rounded w-7/8 mx-auto border-2 h-[50px] mb-auto  border-black" value={password} required />
+		                    <button>Sign In</button>
+		                </form>
+		                {error? 
+		                <p>Invalid login credentials</p>:
+		                <null></null>
+		                }
+		                <p ref={errRef} className={errMess ? "errmess" :
+		                    "offscreen"} aria-live="assertive">{errMess}</p>
+		            </div>
+		            <div className='w-[500px] flex flex-col content-center mx-auto border-solid border-2 h-[600px] p-[30px] bg-zinc-400 rounded-xl mt-[5%]'>
+		                <p ref={errRef} className={errMess ? "errmess" :
+		                    "offscreen"} aria-live="assertive">{errMess}</p>
 
-                            <button>Sign In</button>
-                        </form>
-                        {error? 
-                        <p>Invalid login credentials</p>:
-                        <null></null>
-                        }
-                        <p ref={errRef} className={errMess ? "errmess" :
-                            "offscreen"} aria-live="assertive">{errMess}</p>
-                    </div>
-                    <div className='w-[400px] flex flex-col content-center mx-auto border-solid border-2 h-[500px] p-[20px]'>
-                        <p ref={errRef} className={errMess ? "errmess" :
-                            "offscreen"} aria-live="assertive">{errMess}</p>
+		                <h1 className='text-4xl mb-[20px]'>Register</h1>
 
-                        <h1 className='text-4xl mb-[20px]'>Register</h1>
+		                <form onSubmit={handleRegister} className='w-2/4 md:w-full flex-col flex h-[300px]'>
+		                    <label htmlFor="username" className='mt-auto mb-2'>Username:</label>
+		                    <input type="text" id="username" className= "border-2 border-black w-7/8 mb-auto h-[50px] rounded mx-auto" ref={userRef} autoComplete="off" onChange={(e) => setUserR(e.target.value)} value={userR} required />
 
-                        <form onSubmit={handleRegister} className='w-2/4 md:w-full flex-col flex h-[300px]'>
-                            <label htmlFor="username" className='mt-auto mb-2'>Username:</label>
-                            <input type="text" id="username" className= "border-2 border-black w-7/8 mb-auto h-[50px] rounded mx-auto" ref={userRef} autoComplete="off" onChange={(e) => setUserR(e.target.value)} value={userR} required />
+		                    <label htmlFor="password" className='mt-auto mb-2'>Password:</label>
+		                    <input type="password" id="password" onChange={(e) => setPasswordR(e.target.value)} className= "rounded w-7/8 mx-auto border-2 h-[50px] mb-auto  border-black" value={passwordR} required />
 
-                            <label htmlFor="password" className='mt-auto mb-2'>Password:</label>
-                            <input type="password" id="password" onChange={(e) => setPasswordR(e.target.value)} className= "rounded w-7/8 mx-auto border-2 h-[50px] mb-auto  border-black" value={passwordR} required />
-
-                            <button>Register</button>
-                        </form>
-                        {errorReg? 
-                        <p>An account already exists with this username</p>:
-                        <null></null>
-                        }
-                        <p ref={errRef} className={errMess ? "errmess" :
-                            "offscreen"} aria-live="assertive">{errMess}</p>
-                    </div>
-                </div>
-
+		                    <button>Register</button>
+		                </form>
+		                {errorReg? 
+		                <p>An account already exists with this username</p>:
+		                <null></null>
+		                }
+		                <p ref={errRef} className={errMess ? "errmess" :
+		                    "offscreen"} aria-live="assertive">{errMess}</p>
+		            </div>
+		        </div>
+		</div>
 
             )}
         </>
