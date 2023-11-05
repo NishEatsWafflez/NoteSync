3193ab4b0d5b771ab8d9438c25f468a394dedd5b
Anish Ahuja <ahujaanish11@gmail.com>
2023-11-04 22:51:23 -0700

    Added class page, built routing

--- class-notes-frontend/src/App.js
+++ class-notes-frontend/src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './login'
+import Classes from './classes'
+import { useNavigate } from 'react-router-dom';
+// const navigate = useNavigate();
+
 
 function App() {
-  return (
+  return (  
     <main className = "App">
-      <Login />
-    </main>
+        <Routes>
+          <Route exact path="/" element={<Login/>}/>
+          <Route exact path="/classes" element={<Classes/>}/>
+          {/* <Route exact path="/upcoming/:user" element={<Upcoming/>}/>
+          <Route exact path="/record/:user" element={<Record/>}/> */}
+          {/* <Route path="*" element={<NotFound/>}/> */}
+        </Routes>    </main>
     
   );
 }
--- class-notes-frontend/src/index.js
+++ class-notes-frontend/src/index.js
@@ -4,12 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { AuthProvider } from './context/authProvider';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AuthProvider>
+    <BrowserRouter>
       <App />
+    </BrowserRouter>
+
     </AuthProvider>
   </React.StrictMode>
 );
